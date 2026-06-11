5c3040a25f5ae732ca9b88b4a1a3e0f957c54743
David H. Irving <david.irving@noirlab.edu>
2026-06-10 17:08:14 -0700

    Split up responsibility for datastore remapping
    The datastore remapping is now split between the export and import processes:
    1. Export converts the paths to absolute paths (using its knowledge of the export Butler's datastore configuration)
    2. Import maps those absolute paths to datastores (using its knowledge of the target Butler configuration)
    
    This will make it easier to write tooling to work with the dataset artifacts, by making the complete path to the file available in the export parquet.

--- python/lsst/butler_transform/transform/rewrite_datastore_paths.py
+++ python/lsst/butler_transform/transform/rewrite_datastore_paths.py
@@ -29,6 +29,7 @@ from __future__ import annotations
 
 import re
 from collections.abc import Sequence
+from functools import partial
 from typing import Callable, TypedDict
 
 import pyarrow as pa
@@ -38,44 +39,32 @@ from lsst.daf.butler._rubin.datastore_records import DatastoreRecordTable
 from lsst.daf.butler.arrow_utils import ArrowTableUtils
 
 
-class DatastorePathMapper:
+class AbsolutePathMapper:
     """
-    Utility to rationalize and rewrite paths and datastore names in a Butler
-    datastore dump.
+    Utility to convert paths in a Butler datastore dump to absolute paths.
 
     Parameters
     ----------
     input_datastore_roots
         Mapping from datastore name to root path URI, which will be used
         to convert relative URIs in the datastore records to absolute URIs.
-    output_prefix_datastore_mapping
-        Mapping from absolute URI prefix to datastore name (e.g.
-        ``{ "file://some_root/": "datastore_name" }``, used to assign paths to
-        datastores and convert them to relative paths.
+        Provided automatically if you call ``AbsolutePathMapper.from_butler``
+        to instantiate this class.
 
     Notes
     -----
     Paths in the Butler datastore may be stored as either absolute URLs
     ("direct" ingest mode), or as relative paths mapped under a specified
-    datastore root ("auto"/"copy"/"symlink" and other ingest modes).  Most
-    Butler databases contain a mix of both.
-
-    This class helps normalize these paths by first converting all paths to
-    absolute paths, then assigning them to given datastore names based on
-    their prefix.  This allows the importer to avoid knowing about the specific
-    filesystem layout of the export Butler, since downstream Butlers usually
-    reference files hosted by a service or another copy of the files with
-    a different directory layout.
+    datastore root ("copy"/"symlink" and other ingest modes).  Most
+    Butler databases contain a mix of both, so this class simplifies things
+    downstream by making everything absolute.
     """
 
-    def __init__(
-        self, input_datastore_roots: dict[str, str], output_prefix_datastore_mapping: dict[str, str]
-    ) -> None:
+    def __init__(self, input_datastore_roots: dict[str, str]) -> None:
         self._input_datastore_roots = input_datastore_roots
-        self._output_prefix_datastore_mapping = output_prefix_datastore_mapping
 
     @staticmethod
-    def from_butler(butler: Butler, output_prefix_datastore_mapping: dict[str, str]) -> DatastorePathMapper:
+    def from_butler(butler: Butler) -> AbsolutePathMapper:
         """
         Construct a ``DatastorePathMapper`` with input datastore roots
         retrieved from a Butler configuration.
@@ -84,31 +73,24 @@ class DatastorePathMapper:
         ----------
         butler
             The datastore configuration will be retrieved from this Butler.
-        output_prefix_datastore_mapping
-            Mapping from absolute URI prefix to datastore name (e.g.
-            ``{ "file://some_root/": "datastore_name" }``, used to assign paths
-            to datastores and convert them to relative paths.
         """
-        return DatastorePathMapper(_get_datastore_roots_from_butler(butler), output_prefix_datastore_mapping)
+        return AbsolutePathMapper(_get_datastore_roots_from_butler(butler))
 
-    def remap(self, table: DatastoreRecordTable) -> DatastoreRecordTable:
-        """Return a copy of the given ``DatastoreRecordTable`` with paths and
-        datastore names rewritten.
-
-        Notes
-        -----
-        First, all paths are converted to absolute paths as specified by the
-        ``input_datastore_roots` parameter to this class.
-
-        Second, all paths are converted back to relative paths based on the
-        prefixes in the ``output_prefix_datastore_mapping`` parameter to this
-        class.  The datastore name is set to the value of that mapping.
+    def make_paths_absolute(self, table: DatastoreRecordTable) -> DatastoreRecordTable:
+        """Return a copy of the given ``DatastoreRecordTable`` with all
+        relative paths converted to absolute paths.
         """
         return rewrite_datastore_and_path(table, self._remap_rows)
 
     def _remap_rows(self, rows: Sequence[DatastoreNameAndPath]) -> None:
         _make_uris_absolute(rows, self._input_datastore_roots)
-        _map_uris_to_datastores(rows, self._output_prefix_datastore_mapping)
+
+        """
+        output_prefix_datastore_mapping
+            Mapping from absolute URI prefix to datastore name (e.g.
+            ``{ "file://some_root/": "datastore_name" }``, used to assign paths
+            to datastores and convert them to relative paths.
+        """
 
 
 class DatastoreNameAndPath(TypedDict):
@@ -166,6 +148,25 @@ def _make_uris_absolute(rows: Sequence[DatastoreNameAndPath], datastore_roots: d
                     f"No root known for datastore '{datastore_name}'.  Known roots: {list(datastore_roots.keys())}"
                 )
             row["path"] = f"{root.rstrip('/')}/{path}"
+
+
+def map_absolute_uris_to_datastores(
+    table: DatastoreRecordTable, datastore_map: dict[str, str]
+) -> DatastoreRecordTable:
+    """Make a copy of the given ``DatastoreRecordTable``, converting all paths
+    to relative paths and assigning datastore names based on the
+    ``datastore_map`` parameter.
+
+    Parameters
+    ----------
+    table
+        Input datastore records.
+    datastore_map
+        Mapping from absolute URI prefix to datastore name (e.g.
+        ``{ "file://some_root/": "datastore_name" }``, used to assign paths
+        to datastores and convert them to relative paths.
+    """
+    return rewrite_datastore_and_path(table, partial(_map_uris_to_datastores, datastore_map=datastore_map))
 
 
 def _map_uris_to_datastores(rows: Sequence[DatastoreNameAndPath], datastore_map: dict[str, str]) -> None:
